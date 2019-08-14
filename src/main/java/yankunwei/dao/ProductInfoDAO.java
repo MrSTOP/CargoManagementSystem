9dbe2672971e0b978d50ac539e3794ac6f8ead47
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-08-14 15:40:49 +0800

    添加根据产品ID查询售价的方法

--- src/main/java/yankunwei/dao/ProductInfoDAO.java
+++ src/main/java/yankunwei/dao/ProductInfoDAO.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import yankunwei.utils.DataBaseHelper;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -28,10 +29,35 @@ public class ProductInfoDAO {
                 productIDs.add(resultSet.getInt("ProductID"));
             }
         } catch (SQLException e) {
+            logger.error("Query All ProductID failed");
             e.printStackTrace();
         } finally {
             DataBaseHelper.closeResource(resultSet, preparedStatement, connection);
         }
+        logger.info("Query All ProductID success");
         return productIDs;
+    }
+    
+    public BigDecimal getProductSalePriceByID(int productID) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        BigDecimal price = null;
+        try {
+            connection = DataBaseHelper.getConnection();
+            String SQL = "SELECT \"ProductSalePrice\" FROM \"Product\" WHERE \"ProductID\"=?";
+            preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setInt(1, productID);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                price = resultSet.getBigDecimal("ProductSalePrice");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DataBaseHelper.closeResource(resultSet, preparedStatement, connection);
+        }
+        logger.info("Query productID:{}'s price is {}", productID, price);
+        return price;
     }
 }
