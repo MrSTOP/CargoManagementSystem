5ca549b9d85cb6a60882a3c2171390b72973fde8
YzCatOvO <1315250388@qq.com>
2019-08-19 08:58:50 +0800

    info

--- src/main/java/yanzichao/bean/SupplierInfo.java
+++ src/main/java/yanzichao/bean/SupplierInfo.java
@@ -1,4 +1,54 @@
 package yanzichao.bean;
 
-public class supplierInfo {
+public class SupplierInfo {
+    private long supplierID;
+    private String supplierName;
+    private String supplierDescription;
+    private String supplierAddress;
+    public SupplierInfo() {
+
+    }
+    public SupplierInfo(long supplierID, String supplierName, String supplierDescription, String supplierAddress)
+    {
+        this.supplierID = supplierID;
+        this.supplierName = supplierName;
+        this.supplierDescription = supplierDescription;
+        this.supplierAddress = supplierAddress;
+    }
+    public long getSupplierID() {
+        return supplierID;
+    }
+
+    public void setSupplierID(long supplierID) {
+        this.supplierID = supplierID;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public String getSupplierDescription() {
+        return supplierDescription;
+    }
+
+    public void setSupplierDescription(String supplierDescription) {
+        this.supplierDescription = supplierDescription;
+    }
+
+    public String getSupplierAddress() {
+        return supplierAddress;
+    }
+
+    public void setSupplierAddress(String supplierAddress) {
+        this.supplierAddress = supplierAddress;
+    }
+
+
+
+
+
 }
