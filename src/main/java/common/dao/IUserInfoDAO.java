db3acd326ac48e2503c11706ab3eba0f35177df7
Gobi <1531950593@qq.com>
2019-08-21 10:31:33 +0800

    小细节修改，去除yankunwei中的userInfoDao类，完善UserInfo接口

--- src/main/java/common/dao/IUserInfoDAO.java
+++ src/main/java/common/dao/IUserInfoDAO.java
@@ -1,7 +1,17 @@
 package common.dao;
 
 import java.util.List;
-
+import pengchao.javabean.UserInfo;
 public interface IUserInfoDAO {
-    List<Long> getAllUserID();
+    List<Long> getAllUserID();   //查询全部ID
+
+    UserInfo getUserInfoByID(long userID);  //以ID查询信息
+
+    boolean insertUser(UserInfo userInfo);
+
+    boolean updateUser(UserInfo userInfo);
+
+    boolean deleteUser(UserInfo userInfo);
+
+
 }
