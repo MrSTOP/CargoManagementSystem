ba735a2ba64b37595d96ff6e1930bc82605c13bf
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-08-25 18:49:42 +0800

    修改仓库DAO，删除调整数量的方法，改由存储过程实现

--- src/main/java/common/dao/IStorageDAO.java
+++ src/main/java/common/dao/IStorageDAO.java
@@ -1,11 +1,5 @@
 package common.dao;
 
-import yankunwei.javabean.ProductInfo;
-
 public interface IStorageDAO {
-    boolean insertNewProduct(ProductInfo productInfo);
-    
-    boolean addProductCount(long productID, int count);
-    
-    boolean decProductCount(long productID, int count);
+    boolean canSaleProduct(long productID, int count);
 }
